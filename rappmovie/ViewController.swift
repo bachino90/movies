a775259272e2fe46818cddb740e2a1916a472fe8
bachino90 <emilianobivachi@gmail.com>
2018-12-03 21:12:32 -0300

    Initial Commit

---
+++ rappmovie/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  rappmovie
+//
+//  Created by Restorando on 12/3/18.
+//  Copyright © 2018 BaxLabs. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
