a9ff50cc5a27d18908f4765658b864005b51fa95
bachino90 <emilianobivachi@gmail.com>
2018-12-05 22:41:07 -0300

    Configuration

---
+++ rappmovie/Network/Route.swift
@@ -0,0 +1,48 @@
+//
+//  Router.swift
+//  rappmovie
+//
+//  Created by Emiliano Bivachi on 12/3/18.
+//  Copyright © 2018 BaxLabs. All rights reserved.
+//
+
+import Foundation
+import Alamofire
+
+struct API {
+
+    struct Resource: RouterRequest {
+        let resource: Media.Resource
+        let category: Media.Category
+        let page: Int
+
+        var baseURLString: String { return "https://api.themoviedb.org/3/" }
+
+        var path: String { return "\(resource.rawValue)/\(category.rawValue)" }
+
+        // FOR v4
+        // eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI0NzFhYzZlYTU3M2VhY2RhMDI5YWIwYTczMzgyZTNhNCIsInN1YiI6IjVjMDcwMzA2YzNhMzY4NjVkYzBkMWQ4YiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.7pg36ESJWyjRQKZcgltJ-AKkh6cXpASc8RSjzCYqMBo
+        var parameters: [String : Any]? {
+            return [
+                "api_key": "471ac6ea573eacda029ab0a73382e3a4",
+                "language": "en-US",
+                "page": page,
+            ]
+        }
+    }
+
+    struct Video: RouterRequest {
+        let resource: Media.Resource
+        let id: String
+
+        var path: String { return "\(resource.rawValue)/\(id)/videos" }
+    }
+
+    struct Genre: RouterRequest {
+        let resource: Media.Resource
+
+        var path: String { return "genre/\(resource.rawValue)/list" }
+    }
+}
+
+
