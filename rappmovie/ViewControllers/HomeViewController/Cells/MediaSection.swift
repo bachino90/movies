46cbc47117d6b7d1b6853278e279bfcb144d8fb8
bachino90 <emilianobivachi@gmail.com>
2018-12-06 00:33:40 -0300

    Add title to media carrousel

--- rappmovie/ViewControllers/HomeViewController/Cells/MediaSection.swift
+++ rappmovie/ViewControllers/HomeViewController/Cells/MediaSection.swift
@@ -12,7 +12,7 @@ class MediaSection: Section {
 
     init(_ response: HomeResponse) {
         super.init()
-        rows = response.movies.map { MediaCarrouselRow(mediaCollection: $1.movies) }
-        rows.append(contentsOf: response.tvShows.map { MediaCarrouselRow(mediaCollection: $1.tvShows) })
+        rows = response.movies.map { MediaCarrouselRow(mediaCollection: $1.movies, category: $0) }
+        rows.append(contentsOf: response.tvShows.map { MediaCarrouselRow(mediaCollection: $1.tvShows, category: $0) })
     }
 }
