9acd08f4f8953ac2dfe3e3a236f1336e13d5be2b
bachino90 <emilianobivachi@gmail.com>
2018-12-07 10:51:34 -0300

    Improve models

--- rappmovie/Models/Media.swift
+++ rappmovie/Models/Media.swift
@@ -15,16 +15,31 @@ class Media: Model {
         case popular
         case topRated = "top_rated"
         case upcoming
+
+        var title: String {
+            switch self {
+            case .popular: return "Most Popular"
+            case .topRated: return "Top Rated"
+            case .upcoming: return "Previews"
+            }
+        }
     }
 
     enum Resource: String {
         case movie
         case tvShow = "tv"
+
+        var title: String {
+            switch self {
+            case .movie: return "Movies"
+            case .tvShow: return "TV Shows"
+            }
+        }
     }
 
     let id: Int
     let title: String
-    let posterPath: String?
+    let posterPath: String
     let popularity: Float
     let voteAverage: Float
     let overview: String
@@ -33,7 +48,8 @@ class Media: Model {
     let genreIds: [Int]
     let genres: [Genre]
     let originCountry: [String]
-    let releaseDate: String?
+    let releaseDate: Date?
+    let runTime: Int?
 
     var type: Resource { return .movie }
 
@@ -41,6 +57,7 @@ class Media: Model {
         guard
             let id = json["id"].int,
             let title = json["title"].string ?? json["name"].string,
+            let posterPath = json["poster_path"].string,
             let overview = json["overview"].string,
             let popularity = json["popularity"].float,
             let voteAverage = json["vote_average"].float,
@@ -48,19 +65,20 @@ class Media: Model {
             let voteCount = json["vote_count"].int
         else { return nil }
 
-        self.posterPath = json["poster_path"].string
-
         self.id = id
         self.title = title
+        self.posterPath = posterPath
         self.overview = overview
         self.popularity = popularity
         self.voteAverage = voteAverage
         self.originalLanguage = originalLanguage
         self.voteCount = voteCount
+        self.runTime = json["runtime"].int
         self.genres = json["genres"].arrayValue.compactMap { Genre($0) }
         self.genreIds = json["genre_ids"].arrayValue.compactMap { $0.int }
         self.originCountry = json["origin_country"].arrayValue.map { $0.stringValue }
-        self.releaseDate = json["release_date"].string ?? json["first_air_date"].string
+        let releaseDate = json["release_date"].string ?? json["first_air_date"].string
+        self.releaseDate = DateUtils.sharedInstance.releaseDateFormatter.date(from: releaseDate ?? "")
     }
 
 }
--- rappmovie/Models/TVShow.swift
+++ rappmovie/Models/TVShow.swift
@@ -13,7 +13,13 @@ class TVShow: Media {
 
     override var type: Resource { return .tvShow }
 
+    let numberOfEpisodes: Int?
+    let numberOfSeasons: Int?
+//    let season: [Season]
+
     required init?(_ json: JSON) {
+        numberOfEpisodes = json["number_of_episodes"].int
+        numberOfSeasons = json["number_of_seasons"].int
         super.init(json)
     }
 
