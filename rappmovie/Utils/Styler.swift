94e01d1ffcde0e832ad31e3b570d00b19106661f
bachino90 <emilianobivachi@gmail.com>
2018-12-08 22:31:43 -0300

    Add images

--- rappmovie/Utils/Styler.swift
+++ rappmovie/Utils/Styler.swift
@@ -32,8 +32,8 @@ struct Styler {
     struct Color {
         static let darkBlack = UIColor(red: 10/255.0, green: 10/255.0, blue: 10/255.0, alpha: 1.0)
         static let black = UIColor(red: 20/255.0, green: 20/255.0, blue: 20/255.0, alpha: 1.0)
-        static let lightBlack = UIColor(red: 40/255.0, green: 40/255.0, blue: 40/255.0, alpha: 1.0)
-        static let darkGray = UIColor(red: 60/255.0, green: 60/255.0, blue: 60/255.0, alpha: 1.0)
+        static let lightBlack = UIColor(red: 35/255.0, green: 35/255.0, blue: 35/255.0, alpha: 1.0)
+        static let darkGray = UIColor(red: 50/255.0, green: 50/255.0, blue: 50/255.0, alpha: 1.0)
         static let gray = UIColor.darkGray
         static let lightGray = UIColor(red: 238/255.0, green: 238/255.0, blue: 238/255.0, alpha: 1.0)
         static let darkWhite = UIColor(red: 244/255.0, green: 244/255.0, blue: 244/255.0, alpha: 1.0)
